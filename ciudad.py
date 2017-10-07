a48a02aa6558831c11be279cc08d2ead57786956
Marcelo Iturriaga <muiturriaga@uc.cl>
2017-10-07 15:44:59 -0300

    added ciudad.py :+1:

--- ciudad.py
+++ ciudad.py
@@ -2,12 +2,26 @@ import xlrd
 
 
 
-
 wb = xlrd.open_workbook("Datos.xlsx")
-historico = wb.sheet_by_name("Histórico de basura")
+ciudad = wb.sheet_by_name("Ciudad")
 
-print(historico.cell(3, 3).value)
-print(historico.cell(2, 3).value)
+print("filas: {}, columas: {}".format(ciudad.nrows, ciudad.ncols))
+
+max_basura = {}
+
+for row_idx in range(3, ciudad.nrows):    # Iterate through rows
+    numero_calle = ciudad.cell(row_idx, 1).value
+    x1 = ciudad.cell(row_idx, 2).value
+    y1 = ciudad.cell(row_idx, 3).value
+    x2 = ciudad.cell(row_idx, 4).value
+    y2 = ciudad.cell(row_idx, 5).value
+    maxb = ciudad.cell(row_idx, 6).value
+    max_basura[((x1, y1),(x2, y2))] = maxb
+    max_basura[((x2, y2),(x1, y1))] = maxb
+
+##################################################################################################################################
+#ciudad[numero_calle] = {'arista1':((x1, y1), (x2, y2)), 'arista2':((x2, y2), (x1, y1)), 'basura_actual': 0, 'basura_max': maxb}#
+##################################################################################################################################
 
 # num_cols = historico.ncols   # Number of columns
 # for row_idx in range(0, historico.nrows):    # Iterate through rows
--- read_data.py
+++ read_data.py
@@ -4,25 +4,11 @@ import xlrd
 
 
 wb = xlrd.open_workbook("Datos.xlsx")
-ciudad = wb.sheet_by_name("Ciudad")
+historico = wb.sheet_by_name("Histórico de basura")
 
-print("filas: {}, columas: {}".format(ciudad.nrows, ciudad.ncols))
+print(historico.cell(3, 3).value)
+print(historico.cell(2, 3).value)
 
-max_basura = {}
-
-for row_idx in range(3, ciudad.nrows):    # Iterate through rows
-    numero_calle = ciudad.cell(row_idx, 1).value
-    x1 = ciudad.cell(row_idx, 2).value
-    y1 = ciudad.cell(row_idx, 3).value
-    x2 = ciudad.cell(row_idx, 4).value
-    y2 = ciudad.cell(row_idx, 5).value
-    maxb = ciudad.cell(row_idx, 6).value
-    max_basura[((x1, y1),(x2, y2))] = maxb
-    max_basura[((x2, y2),(x1, y1))] = maxb
-
-##################################################################################################################################
-#ciudad[numero_calle] = {'arista1':((x1, y1), (x2, y2)), 'arista2':((x2, y2), (x1, y1)), 'basura_actual': 0, 'basura_max': maxb}#
-##################################################################################################################################
 
 #    print ('Row: %s' % row_idx)   # Print row number
 #    for col_idx in range(0, num_cols):  # Iterate through columns
